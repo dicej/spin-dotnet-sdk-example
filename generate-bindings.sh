b28371e105cf0ff757b97ec1f22dba36f8f4968a
Joel Dice <joel.dice@fermyon.com>
2024-08-01 12:06:00 -0600

    snapshot
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -3,6 +3,6 @@
 set -ex
 
 # TODO: update to next release containing https://github.com/bytecodealliance/wit-bindgen/pull/1017
-cargo install --locked --no-default-features --features csharp --git https://github.com/dicej/wit-bindgen --rev dd0073f1 wit-bindgen-cli --root $(pwd)
+cargo install --locked --no-default-features --features csharp --git https://github.com/dicej/wit-bindgen --rev e9430c5a wit-bindgen-cli --root $(pwd)
 ./bin/wit-bindgen c-sharp -w spin-http -r native-aot wit
 rm SpinHttpWorld_component_type.* SpinHttpWorld_cabi_realloc.c SpinHttpWorld_wasm_import_linkage_attribute.cs SpinHttpWorld.wit.imports.* SpinHttp.cs
